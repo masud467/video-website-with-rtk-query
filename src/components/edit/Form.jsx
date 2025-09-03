9e304cc5f510fa2e1e2ee196690f63991d14c066
masud467 <masudpfrnt@gmail.com>
2025-09-03 12:35:09 +0600

    add Edit page with form to update existing video.

---
+++ src/components/edit/Form.jsx
@@ -0,0 +1,146 @@
+import { useEditVideoMutation } from "../../features/api/videoApi";
+import { useState } from "react";
+import TextInput from "../ui/TextInput";
+import TextArea from "../ui/TextArea";
+import Success from "../ui/Success.jsx";
+import Error from "../ui/Error.jsx";
+
+const Form = ({ video }) => {
+  const {
+    id,
+    title: initialTitle,
+    author: initialAuthor,
+    description: initialDescription,
+    link: initialLink,
+    thumbnail: initialThumbnail,
+    date: initialDate,
+    duration: initialDuration,
+    views: initialViews,
+  } = video;
+
+  const [editVideo, { isLoading, isError, isSuccess }] = useEditVideoMutation();
+
+  const [title, setTitle] = useState(initialTitle);
+  const [author, setAuthor] = useState(initialAuthor);
+  const [description, setDescription] = useState(initialDescription);
+  const [link, setLink] = useState(initialLink);
+  const [thumbnail, setThumbnail] = useState(initialThumbnail);
+  const [date, setDate] = useState(initialDate);
+  const [duration, setDuration] = useState(initialDuration);
+  const [views, setViews] = useState(initialViews);
+
+  // const resetForm = () => {
+  //   setTitle("");
+  //   setAuthor("");
+  //   setDescription("");
+  //   setLink("");
+  //   setThumbnail("");
+  //   setDate("");
+  //   setDuration("");
+  //   setViews("");
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    editVideo({
+      id,
+      data: {
+        title,
+        author,
+        description,
+        link,
+        thumbnail,
+        date,
+        duration,
+        views,
+      },
+    });
+    // resetForm();
+  };
+  return (
+    <form method="PATCH" onSubmit={handleSubmit}>
+      <div className="shadow overflow-hidden sm:rounded-md">
+        <div className="px-4 py-5 bg-white sm:p-6">
+          <div className="grid grid-cols-6 gap-6">
+            <div className="col-span-6 sm:col-span-3">
+              <TextInput
+                title="Video Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-3">
+              <TextInput
+                title="Author"
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+              />
+            </div>
+
+            <div className="col-span-6">
+              <TextArea
+                title="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+
+            <div className="col-span-6">
+              <TextInput
+                title="Video Link"
+                value={link}
+                onChange={(e) => setLink(e.target.value)}
+              />
+            </div>
+
+            <div className="col-span-6">
+              <TextInput
+                title="Thumbnail Link"
+                value={thumbnail}
+                onChange={(e) => setThumbnail(e.target.value)}
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+              <TextInput
+                title="Date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+              <TextInput
+                title="Duration"
+                value={duration}
+                onChange={(e) => setDuration(e.target.value)}
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+              <TextInput
+                title="Views"
+                value={views}
+                onChange={(e) => setViews(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+          <button
+            type="submit"
+            disabled={isLoading}
+            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-indigo-500"
+          >
+            Edit
+          </button>
+        </div>
+        {isSuccess && <Success message="Video was edited successfully" />}
+        {isError && <Error message="There was an error!" />}
+      </div>
+    </form>
+  );
+};
+
+export default Form;
